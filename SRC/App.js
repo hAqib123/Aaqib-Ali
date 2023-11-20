3dc1bf8364e01f20dc45986024e389928bb2432d
hAqib123 <hkrajri786@gmail.com>
2023-11-20 18:52:09 +0500

    difficult aa

---
+++ SRC/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import Welcome from './components/Welcome';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
